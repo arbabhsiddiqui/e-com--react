05dae9dad40aaee6ab0908bd64967ae74df7cba9
arbabhsiddiqui <zackh41@gmail.com>
2020-04-11 12:09:08 +0530

    update project with reselect inorder to save unwanted reload

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,7 +7,7 @@ import CustomButton from "../custom-button/custom-button.componenet";
 import "../../redux/cart/cart.reducer";
 
 import CartItem from "../cart-item/cart-item.component";
-
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import "./cart-dropdown.styles.scss";
 
 const CartDropDown = ({ cartItems }) => (
@@ -21,8 +21,8 @@ const CartDropDown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropDown);
