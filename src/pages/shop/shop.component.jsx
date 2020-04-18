ec970e68754f77ff1219cdaab15f2808f91a3bde
arbabhsiddiqui <zackh41@gmail.com>
2020-04-18 15:28:05 +0530

    move collection,directory into its own redux files and create other file for category

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./shop.styles.scss";
-import SHOP_DATA from "./shop.data.js";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-class Shop extends React.Component {
-  constructor(props) {
-    super(props);
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-    this.State = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.State;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Shop = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default Shop;
